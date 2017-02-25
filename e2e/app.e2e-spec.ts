e18622e6b0fec3193928dcbb1dcc5d0833be06bb
Angular CLI <angular-cli@angular.io>
2017-02-25 16:35:11 +0100

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Ee5dashboardPage } from './app.po';
+
+describe('ee5dashboard App', () => {
+  let page: Ee5dashboardPage;
+
+  beforeEach(() => {
+    page = new Ee5dashboardPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class Ee5dashboardPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
